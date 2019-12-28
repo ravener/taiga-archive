355893358fb2aa10d3f30ebca99dba95c05c5fd7
pollen5 <mohammed.ahmed.minecraft@gmail.com>
2019-12-28 11:24:58 +0430

    this schema isn't needed anymore

--- db/schema.go
+++ db/schema.go
@@ -2,10 +2,6 @@ package db
 
 // SQL Queries to create the schemas.
 const SCHEMA = `
-CREATE TABLE guildcreate (
-  id TEXT PRIMARY KEY NOT NULL UNIQUE
-);
-
 CREATE TABLE guilds (
   id TEXT PRIMARY KEY NOT NULL UNIQUE,
   prefix TEXT DEFAULT 't!'
